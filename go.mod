3b9a5753932903516bdb5c6e44746fab9938a78f
LeBronQ <61304029+LeBronQ@users.noreply.github.com>
2024-06-29 16:21:58 +0800

    kdtree tasks

--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.22.4
 require github.com/hibiken/asynq v0.24.1
 
 require (
+	github.com/LeBronQ/kdtree v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/kyroy/priority-queue v0.0.0-20180327160706-6e21825e7e0c // indirect
 	github.com/redis/go-redis/v9 v9.5.3 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
--- task.go
+++ task.go
@@ -4,25 +4,20 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
-	"time"
 
+	"github.com/LeBronQ/kdtree"
 	"github.com/hibiken/asynq"
 )
+
+type KDTree kdtree.KDTree
 
 // A list of task types.
 const (
 	TypeEmailDelivery = "email:deliver"
-	TypeImageResize   = "image:resize"
 )
 
-type EmailDeliveryPayload struct {
-	UserID     int
-	TemplateID string
-}
-
-type ImageResizePayload struct {
-	SourceURL string
+type KDtreeDeliveryPayload struct {
+	tree *KDTree
 }
 
 //----------------------------------------------
@@ -30,21 +25,12 @@ type ImageResizePayload struct {
 // A task consists of a type and a payload.
 //----------------------------------------------
 
-func NewEmailDeliveryTask(userID int, tmplID string) (*asynq.Task, error) {
-	payload, err := json.Marshal(EmailDeliveryPayload{UserID: userID, TemplateID: tmplID})
+func NewKDtreeDeliveryTask(t *KDTree) (*asynq.Task, error) {
+	payload, err := json.Marshal(KDtreeDeliveryPayload{tree: t})
 	if err != nil {
 		return nil, err
 	}
 	return asynq.NewTask(TypeEmailDelivery, payload), nil
-}
-
-func NewImageResizeTask(src string) (*asynq.Task, error) {
-	payload, err := json.Marshal(ImageResizePayload{SourceURL: src})
-	if err != nil {
-		return nil, err
-	}
-	// task options can be passed to NewTask, which can be overridden at enqueue time.
-	return asynq.NewTask(TypeImageResize, payload, asynq.MaxRetry(5), asynq.Timeout(20*time.Minute)), nil
 }
 
 //---------------------------------------------------------------
@@ -55,31 +41,12 @@ func NewImageResizeTask(src string) (*asynq.Task, error) {
 // that satisfies asynq.Handler interface. See examples below.
 //---------------------------------------------------------------
 
-func HandleEmailDeliveryTask(ctx context.Context, t *asynq.Task) error {
-	var p EmailDeliveryPayload
+func HandleKDtreeDeliveryTask(ctx context.Context, t *asynq.Task) error {
+	var p KDtreeDeliveryPayload
 	if err := json.Unmarshal(t.Payload(), &p); err != nil {
 		return fmt.Errorf("json.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
 	}
-	log.Printf("Sending Email to User: user_id=%d, template_id=%s", p.UserID, p.TemplateID)
+	fmt.Println("kdtree:", p.tree)
 	// Email delivery code ...
 	return nil
-}
-
-// ImageProcessor implements asynq.Handler interface.
-type ImageProcessor struct {
-	// ... fields for struct
-}
-
-func (processor *ImageProcessor) ProcessTask(ctx context.Context, t *asynq.Task) error {
-	var p ImageResizePayload
-	if err := json.Unmarshal(t.Payload(), &p); err != nil {
-		return fmt.Errorf("json.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
-	}
-	log.Printf("Resizing image: src=%s", p.SourceURL)
-	// Image resizing code ...
-	return nil
-}
-
-func NewImageProcessor() *ImageProcessor {
-	return &ImageProcessor{}
 }
