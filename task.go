dd0b6b67c9076646607d16caaf09df050e343c9f
LeBronQ <61304029+LeBronQ@users.noreply.github.com>
2024-06-30 17:20:44 +0800

    Update task.go

--- task.go
+++ task.go
@@ -1,11 +1,9 @@
 package tasks
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 
-	"github.com/LeBronQ/kdtree"
 	"github.com/hibiken/asynq"
 )
 
@@ -14,8 +12,17 @@ const (
 	TypeKDtreeDelivery = "kdtree:deliver"
 )
 
+type TreeNodeData struct {
+	ID int64
+}
+
+type DeliveryPoint struct {
+	Coordinates []float64 `json:"coordinates"`
+	ID          int64     `json:"id"`
+}
+
 type KDtreeDeliveryPayload struct {
-	Tree *kdtree.KDTree
+	TreeNodes []DeliveryPoint
 }
 
 //----------------------------------------------
@@ -23,30 +30,11 @@ type KDtreeDeliveryPayload struct {
 // A task consists of a type and a payload.
 //----------------------------------------------
 
-func NewKDtreeDeliveryTask(t *kdtree.KDTree) (*asynq.Task, error) {
-	payload, err := json.Marshal(KDtreeDeliveryPayload{Tree: t})
+func NewKDtreeDeliveryTask(t []DeliveryPoint) (*asynq.Task, error) {
+	payload, err := json.Marshal(KDtreeDeliveryPayload{TreeNodes: t})
+	fmt.Print("p:", string(payload))
 	if err != nil {
 		return nil, err
 	}
-	fmt.Print(payload)
 	return asynq.NewTask(TypeKDtreeDelivery, payload), nil
-}
-
-//---------------------------------------------------------------
-// Write a function HandleXXXTask to handle the input task.
-// Note that it satisfies the asynq.HandlerFunc interface.
-//
-// Handler doesn't need to be a function. You can define a type
-// that satisfies asynq.Handler interface. See examples below.
-//---------------------------------------------------------------
-
-func HandleKDtreeDeliveryTask(ctx context.Context, t *asynq.Task) error {
-	var p KDtreeDeliveryPayload
-	fmt.Print("payload:", t.Payload())
-	if err := json.Unmarshal(t.Payload(), &p); err != nil {
-		return fmt.Errorf("json.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
-	}
-	fmt.Println("kdtree:", p.Tree)
-	// Email delivery code ...
-	return nil
 }
